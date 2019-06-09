6a62f9b49578eda6b1ec936857b8c856ea9c02e2
liaohongdong <liaodoho@gmail.com>
2019-06-09 13:47:41 +0800

    test

--- src/store/store.js
+++ src/store/store.js
@@ -10,6 +10,7 @@ import index from './modules/index'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // strict: process.env.NODE_ENV !== 'production',
   state: {
     liao: 'haha',
     user: 'liao_user',
